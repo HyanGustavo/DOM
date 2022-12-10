3393c406bfb3a15ca7a0cd527c486e53c4d0afa4
hyan <hyangust@gmail.com>
2022-12-10 17:57:46 -0300

    first commit

---
+++ DOM.js
@@ -0,0 +1,14 @@
+function validarForm() {
+    let nome = document.querySelector("#nome")
+    let email = document.querySelector("#email")
+
+    if (nome.vaule != "" && e email.vaule != "") {
+       alert("o campo esta vazio")
+    
+    alert("o campo ta cheio")
+} 
+}
+
+function validartheme() {
+    document.body.classList.toggle("dark");
+}
